ee17d36681ec9cea6875756703acc2f0dc8bb4c7
Misha Topchilo <miksoft.tm@gmail.com>
2025-03-15 21:00:34 -0700

    Updated fetch photo (#7)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- build.js
+++ build.js
@@ -3,13 +3,12 @@ const fs = require('fs')
 const axios = require('axios')
 const today = new Date().toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
-const result = axios.get('https://api.astro.miksoft.pro/photos').then((result) => {
+const result = axios.get('https://api.astro.miksoft.pro/photos?limit=4&order=rand').then((result) => {
     let photoContent = ''
 
     const items = result?.data?.items || [];
-    const randomItems = items.sort(() => 0.5 - Math.random()).slice(0, 4);
 
-    randomItems?.forEach((photo, index) => {
+    items?.forEach((photo, index) => {
         photoContent += (`<img src="${imageHost}${photo.dirName}/${photo.fileName}_medium.${photo.fileExt}" alt="" style="width: 24%; height: 150px; object-fit: cover;" />`)
 
         if ((index + 1) < result.data.items.length) {
